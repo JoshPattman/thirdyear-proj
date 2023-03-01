43f02c0ab9e5f8f14d4ee36d181e042560d943e8
Josh Pattman <Josh.pattman@gmail.com>
2023-03-01 15:03:05 +0000

    added some implementation

---
+++ comp3200-cw/v3/just_model_mnist.py
@@ -0,0 +1,17 @@
+from model import make_clone_model, get_xy, make_model
+import numpy as np
+
+(train_X, train_Y), (test_X, test_Y) = get_xy(60000)
+
+model = make_clone_model()
+
+model.fit(train_X, train_Y, epochs=5, verbose=True)
+
+preds = model.predict(test_X, verbose=False)
+num_correct = 0
+for i in range(len(test_Y)):
+    if np.argmax(preds[i]) == test_Y[i]:
+        num_correct += 1
+accuracy = 100*num_correct/test_Y.shape[0]
+
+print("ACCURACY, %s"%accuracy)
