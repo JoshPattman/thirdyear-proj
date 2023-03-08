296c8ec29dc9518fdd3eb9df5a2fe7ac82564ee5
Josh Pattman <Josh.pattman@gmail.com>
2023-03-08 09:05:48 +0000

    more parameters

--- comp3200-cw/v3/ex4.sh
+++ comp3200-cw/v3/ex4.sh
@@ -9,7 +9,7 @@ numsamples=6000
 for i in {1..5}; do
     # 0.5 0.7 0.9 1 AVG
     for alpha in 0.5 0.7 0.9 1; do
-        for beta in 9999 0 -0.5; do
+        for beta in 0.5; do
             for gamma in 0 3 5 8; do
                 python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma "ex4B_($alpha)_($beta)_($gamma)"
             done
