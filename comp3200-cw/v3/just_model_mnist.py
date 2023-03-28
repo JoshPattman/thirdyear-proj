6a83b917e4f90ab184788bb36dbd66b55b1473a8
Josh Pattman <Josh.pattman@gmail.com>
2023-03-28 12:02:02 +0100

    stuff

--- comp3200-cw/v3/just_model_mnist.py
+++ comp3200-cw/v3/just_model_mnist.py
@@ -1,11 +1,11 @@
 from model import make_clone_model, get_xy, make_model
 import numpy as np
 
-(train_X, train_Y), (test_X, test_Y) = get_xy(60000)
+(train_X, train_Y), (test_X, test_Y) = get_xy(6000)
 
 model = make_clone_model()
 
-model.fit(train_X, train_Y, epochs=5, verbose=True)
+model.fit(train_X, train_Y, epochs=20, verbose=True)
 
 preds = model.predict(test_X, verbose=False)
 num_correct = 0
