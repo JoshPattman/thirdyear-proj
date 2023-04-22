8758b8535b331ced0feb625aedc1eeeffc93f446
Josh Pattman <Josh.pattman@gmail.com>
2023-04-22 10:28:22 +0100

    One full run of data

--- comp3200-cw/v3/experiments.sh
+++ comp3200-cw/v3/experiments.sh
@@ -1,126 +1,71 @@
 #!/bin/bash
 
-numnodes=10
-startupdelay=0
-
 # Benchmark - See what the performance should be with just one node
 #python3 just_model_mnist.py
 
 # Tests
-# 0-2 - FL Dense
-# 3-5 - SL Dense
-# 6-8 - SL sparse 0.75
-# 9-11 - SL sparse 0.5
-# 12-14 - SL sparse 0.25
-# 15-17 - SL sparse 0
+# 0-2 FL Dense
+# 3-5 SL Dense
+# 6-8 SL Sparse 0.75
+# 9-11 SL Sparse 0.5
+# 12-14 SL Sparse 0.25
+# 15-17 SL Sparse 0.0
 
+export numnodes=10 startupdelay=0
 
-## ------------------------ FL Dense ------------------------
-export numsamples=25 eps=2
-for i in {1..5}; do
+export alpha=0.75 beta=0.5
+
+for i in 2; do
+    # FL Dense
+    export numsamples=1000 eps=5
     python3 fed_mnist.py $numnodes $numsamples $eps test0.$i
-done
-#
-#export numsamples=1000 eps=5
-#for i in {1..5}; do
-#    python3 fed_mnist.py $numnodes $numsamples $eps test1.$i
-#done
-#
-#export numsamples=100 eps=15
-#for i in {1..5}; do
-#    python3 fed_mnist.py $numnodes $numsamples $eps test2.$i
-#done
-#
-## ------------------------ SL Dense ------------------------
-export density=1.0 alpha=0.75 beta=0.5 gamma=8
+    export numsamples=100 eps=10
+    python3 fed_mnist.py $numnodes $numsamples $eps test1.$i
+    export numsamples=25 eps=20
+    python3 fed_mnist.py $numnodes $numsamples $eps test2.$i
 
-export numsamples=25 eps=2
-for i in {1..5}; do
+    # SL Dense
+    export density=1.0 gamma=8
+    export numsamples=1000 eps=5
     python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test3.$i
-done
-#
-#export numsamples=1000 eps=5
-#for i in {1..5}; do
-#    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test4.$i
-#done
-#
-#export numsamples=100 eps=15
-#for i in {1..5}; do
-#    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test5.$i
-#done
-#
-## ------------------------ SL sparse 0.75 ------------------------
-## Mean min steps, Mean cons per node = 1.2, 7.2
-export density=0.75 alpha=0.9 beta=0.5 gamma=7
+    export numsamples=100 eps=10
+    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test4.$i
+    export numsamples=25 eps=20
+    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test5.$i
 
-export numsamples=25 eps=2
-for i in {1..5}; do
+    # SL Sparse 0.75
+    export density=0.75 gamma=7
+    export numsamples=1000 eps=5
     python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test6.$i
-done
-#
-#export numsamples=1000 eps=5
-#for i in {1..5}; do
-#    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test7.$i
-#done
-#
-#export numsamples=100 eps=15
-#for i in {1..5}; do
-#    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test8.$i
-#done
-#
-## ------------------------ SL sparse 0.5 ------------------------
-## Mean min steps, Mean cons per node = 1.4, 5.4
-export density=0.5 alpha=0.9 beta=0.5 gamma=5
+    export numsamples=100 eps=10
+    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test7.$i
+    export numsamples=25 eps=20
+    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test8.$i
 
-export numsamples=25 eps=2
-for i in {1..5}; do
+    # SL Sparse 0.5
+    export density=0.5 gamma=5
+    export numsamples=1000 eps=5
     python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test9.$i
-done
-#
-#export numsamples=1000 eps=5
-#for i in {1..5}; do
-#    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test10.$i
-#done
-#
-#export numsamples=100 eps=15
-#for i in {1..5}; do
-#    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test11.$i
-#done
-#
-## ------------------------ SL sparse 0.25 ------------------------
-## Mean min steps, Mean cons per node = 1.7, 3.6
-export density=0.25 alpha=0.9 beta=0.5 gamma=3
+    export numsamples=100 eps=10
+    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test10.$i
+    export numsamples=25 eps=20
+    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test11.$i
 
-export numsamples=25 eps=2
-for i in {1..5}; do
+    # SL Sparse 0.25
+    export density=0.5 gamma=3
+    export numsamples=1000 eps=5
     python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test12.$i
-done
-#
-#export numsamples=1000 eps=5
-#for i in {1..5}; do
-#    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test13.$i
-#done
-#
-#export numsamples=100 eps=15
-#for i in {1..5}; do
-#    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test14.$i
-#done
-#
-## ------------------------ SL sparse 0 ------------------------
-## Mean min steps, Mean cons per node = 3.0, 1.8
-export density=0 alpha=0.9 beta=0.5 gamma=1
+    export numsamples=100 eps=10
+    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test13.$i
+    export numsamples=25 eps=20
+    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test14.$i
 
-export numsamples=25 eps=2
-for i in {1..5}; do
+    # SL Sparse 0.0
+    export density=0.0 gamma=1
+    export numsamples=1000 eps=5
     python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test15.$i
+    export numsamples=100 eps=10
+    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test16.$i
+    export numsamples=25 eps=20
+    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test17.$i
 done
-#
-#export numsamples=1000 eps=5
-#for i in {1..5}; do
-#    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test16.$i
-#done
-#
-#export numsamples=100 eps=15
-#for i in {1..5}; do
-#    python3 swarm_mnist.py $numnodes $startupdelay $numsamples $alpha $beta $gamma $density $eps test17.$i
-#done
